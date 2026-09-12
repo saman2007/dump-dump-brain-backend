45c6d8c42ce9a913b3541daa2a08a1da7fac4714
sami_dev <samanahrari@yahoo.com>
2026-09-12 12:24:35 +0330

    feat: added db config files using drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./src/db/models/*",
+  out: "./src/db/migrations",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+    ssl: process.env.DATABASE_SUPPORT_SSL === "true",
+  },
+});
