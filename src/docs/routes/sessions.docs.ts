58af34163913986df4040d65c3df5fc69fa2cfc3
sami_dev <samanahrari@yahoo.com>
2026-09-24 18:06:55 +0330

    feat: added revoke session docs

--- src/docs/routes/sessions.docs.ts
+++ src/docs/routes/sessions.docs.ts
@@ -1,10 +1,21 @@
 import z from "zod";
-import { registry } from "../openapi.js";
+import { getApiMdFile, registry } from "../openapi.js";
 import { authComponent, jwtErrorResponse } from "../schemas.js";
 
+const renderSessionSchema = z.object({
+  id: z.string(),
+  createdAt: z.date(),
+  userId: z.string(),
+  userAgent: z.string().nullable(),
+  ipAddress: z.string().nullable(),
+  expiresAt: z.date(),
+});
+
+// /sessions docs
 registry.registerPath({
   method: "get",
   path: "/sessions",
+  summary: "/sessions",
   tags: ["Sessions"],
   security: [{ [authComponent.name]: [] }],
   request: { cookies: z.object({ access_token: z.string() }) },
@@ -15,17 +26,35 @@ registry.registerPath({
         "application/json": {
           schema: z.object({
             success: z.literal(true),
-            data: z.array(
-              z.object({
-                id: z.string(),
-                createdAt: z.date(),
-                userId: z.string(),
-                userAgent: z.string().nullable(),
-                ipAddress: z.string().nullable(),
-                expiresAt: z.date(),
-              }),
-            ),
+            data: z.array(renderSessionSchema),
             message: z.null(),
+          }),
+        },
+      },
+    },
+    401: { $ref: `#/components/responses/${jwtErrorResponse.name}` },
+  },
+});
+
+// /sessions/revoke docs
+registry.registerPath({
+  method: "delete",
+  path: "/sessions/revoke",
+  summary: "/sessions/revoke",
+  security: [{ [authComponent.name]: [] }],
+  tags: ["Sessions"],
+  request: { cookies: z.object({ access_token: z.string() }) },
+  description: getApiMdFile("revoke-sessions"),
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: z.object({
+            success: z.literal(true),
+            data: renderSessionSchema.openapi({
+              description: "The revoked session",
+            }),
+            message: z.literal("Revoked the session successfully."),
           }),
         },
       },
